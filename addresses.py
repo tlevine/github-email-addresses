a1f806121d4d3a902f4434a65a2967f4cce0daf2
Thomas Levine <_@thomaslevine.com>
2015-01-31 03:21:57 +0000

    weird

--- addresses.py
+++ addresses.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
-import requests
+import os, json, pickle
+import yaml, requests, vlermv
 
 members_url = 'https://api.github.com/orgs/cyberwizardinstitute/members'
+hub = os.path.expanduser('~/.config/hub')
+headers = {
+    'Authorization': 'token %s' % yaml.load(open(hub))['github.com'][0]['oauth_token']
+}
 
 def main():
     import csv, sys
@@ -9,26 +14,29 @@ def main():
     w.writerow(('name', 'email_address'))
     w.writerows(itermembers())
 
+@vlermv.cache('~/.github-email-addresses', serializer = pickle)
+def get(url):
+    return json.loads(requests.get(url, headers = headers).text)
+
 def itermembers():
-    for member in requests.get(members_url).json():
+    members = get(members_url)
+    for member in members:
         done = False
-        repositories = requests.get(member['repos_url']).json()
+        repositories = get(member['repos_url'])
         for repository in repositories:
             if done:
                 break
             commits_url = repositories[0]['commits_url'].replace('{/sha}', '')
-            commits = requests.get(commits_url).json()
+            commits = get(commits_url)
             for commit in commits:
-                if done or commit == None:
+                if done or commit == None or type(commit) == str:
                     continue
-
                 real_commit = commit['commit']
                 author = real_commit['author']
                 email = author['email']
                 if email != None and 'no-reply' not in email:
                     yield real_commit.get('name', member['login']), email
                     done = True
-        break
 
 if __name__ == '__main__':
     main()
